3e1c529825f1a486b582dfa29c2b0da671add5c3
Michael Dresler <michaeldresler@hotmail.com>
2025-02-05 14:49:45 -0800

    update

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -1,72 +1,24 @@
 
 
-// type ProjectHeroProps = {
-//   title: string;
-//   Image?: null;
-//   timeline: string;
-//   role: string;
-//   names: string[];
-// };
-// export const ProjectHero = ({
-//   title,
-//   timeline,
-//   role,
-//   names,
-// }: ProjectHeroProps) => {
-//   return (
-//     <>
-    
-//     <div className="relative aspect-square sm:aspect-[2.2/1] bg-indigo-700/20 rounded-3xl flex justify-center overflow-hidden ">
-//     <div className=" absolute w-full h-full  bg-opacity-10 bg-[radial-gradient(circle_at_bottom,_var(--tw-gradient-stops))] from-40% from-indigo-600/30 to-indigo-400/10 to-80% backdrop-blur-sm "></div>
-//     {/* <Image
-//       src={mike}
-//       alt="hello"
-//       sizes="100vw"
-//       className=" w-full object-cover "
-//         /> */}
+type timelineProps = {
+  date: string;
+  description: string;
+  isLast?:boolean;
+};
+export const Timeline = ({
+  date,
+  description,
+  isLast
+}: timelineProps) => {
+  return (
+    <>
+    <div className={`h-20 pl-6 ml-2 relative ${isLast ? '' : 'border-l border-black/50 border-dashed'}`}>
+        <div className="w-4 h-4 bg-[#577D4E] absolute -left-2 top-0 rotate-45"></div>
+        <div>{date}</div>
+        <p>{description}</p>
+    </div>
 
-//     </div>
-
-
-
-
-//       <div className="text-text-primary grid grid-cols-1 md:grid-cols-[0.5fr_0.6fr_1.25fr_0.5fr] mt-12">
-//         <div className="clamp2">{title}</div>
-//         <div className="md:col-start-3 md:col-span-full">
-//           <ul>
-//             <div className="flex flex-col md:flex-row justify-between gap-4 py-4 md:py-[1.5em] border-b border-foreground/20">
-//               <li>Client </li>
-//               <li className="clamp ">MoneyMaven </li>
-//             </div>
-//             <div className="flex flex-col md:flex-row justify-between gap-4 py-4 md:py-[1.5em] border-b border-foreground/20">
-//               <li>Year</li>
-//               <li className="clamp">{timeline}</li>
-//             </div>
-//             <div className="flex flex-col md:flex-row justify-between gap-4 py-4 md:py-[1.5em] border-b border-foreground/20">
-//               <li>Role </li>
-//               <li className="clamp">{role}</li>
-//             </div>
-//             <div className="flex flex-col md:flex-row justify-between gap-4 py-4 md:py-[1.5em] border-b border-foreground/20">
-//               <li>Team </li>
-//               <li className="max-w-[20ch] clamp text-left md:text-right">
-//                 {names.map((name, index) => (
-//                   <div key={index}>{name}</div>
-//                 ))}
-//               </li>
-//             </div>
-//           </ul>
-//           <p className="my-[3.5em] text-[26px] leading-[130%]">
-//             FundMate is a fintech platform that allows users to manage their
-//             finances in an intuitive and hassle-free manner. The platform offers
-//             a range of services including budget tracking, bill payments,
-//             investment management, and financial advice. Overall, the goal of
-//             the SavvyFin platform is to provide a user-friendly and customizable
-//             solution for managing personal finances, investments, and business
-//             finances. The platform aims to simplify financial management and
-//             empower users to make informed financial decisions.
-//           </p>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
+   
+    </>
+  );
+};
--- app/scarf/page.tsx
+++ app/scarf/page.tsx
@@ -1,6 +1,7 @@
 
 import Image from "next/image";
 import placeholder from "@/public/placeholder.png"
+import { Timeline } from "@/app/components/Timeline"
 
 export default function scarf() {
 
@@ -8,15 +9,28 @@ export default function scarf() {
       <div>
         <main className="  ">
 
-          <h1>title of clothing</h1>
+        <h2 className="text-[#577D4E] font-medium text-2xl tracking-tighter pb-2">Red Scarf</h2>
           <Image
           src={placeholder}
           alt = "alt text"
+          className="pb-8"
           />
-          <h2>Biography</h2>
+
+          <h2 className="text-[#577D4E] font-medium text-xl tracking-tighter pb-2">Biography</h2>
+
           <p>On a cold winter day in December 2001, the Red Scarf was gifted to a girl from her roommate, in celebration of her 21st birthday. The scarf would then make occasional appearances when the weather got cooler. As time went on, the student graduated and moved out, leaving the scarf behind in her old home. Many years later, the student makes a trip back home to visit her parents with her new family and rediscovers her old scarf. She subsequently gifts it to her daughter, who is also heading to University. </p>
 
-          <h2>memories</h2>
+
+
+
+          <div className="pt-8">
+          <h2 className="text-[#577D4E] font-medium text-xl tracking-tighter pb-4">Memories</h2>
+          <Timeline date="1999" description="Sent off a close friend to Airport"/>
+          <Timeline date="1999" description="Went on a date with the one"/>
+          <Timeline date="1999" description="Won a massive snowball fight"/>
+          <Timeline isLast={true} date="1999" description="Custom embroidery added by grandmother"/>
+          </div>
+        
           
 
         
