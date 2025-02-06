f4ca2025b4b6189cb0b6cdfc9fb2bcab6d14fce5
Michael Dresler <michaeldresler@hotmail.com>
2025-02-06 00:45:26 -0800

    top done

--- app/TShirt_202501_A1/page.tsx
+++ app/TShirt_202501_A1/page.tsx
@@ -1,15 +1,54 @@
-// import Image from "next/image";
-// import Link from "next/link";
-// import { geistMono, svgIcons } from "../fonts";
 
+import Image from "next/image";
+import shirtimg from "@/public/shirt.jpg"
+import { Timeline } from "@/app/components/Timeline"
+import { StatCard } from "../components/StatCard";
 
-export default function TShirt() {
+export default function shirt() {
 
     return (
       <div>
-        <main className="grid grid-cols-1 md:grid-cols-2 md:gap-24 w-full h-screen  ">
-          hello
+        <main className="pb-24 flex flex-col gap-10   ">
 
+
+        <section>
+        <h2 className="text-[#577D4E] font-semibold text-2xl tracking-tight pb-2">Women's Halter</h2>
+          
+          <Image
+          src={shirtimg}
+          alt = "alt text"
+          className=" bg-[#F8F9F6] aspect-[1/1.21] object-contain object-[10%_50%] w-full"
+          />
+
+        </section>
+
+        <section className="">
+        <h2 className="text-[#577D4E] font-semibold text-xl tracking-tight pb-2">Biography</h2>
+          <p>No information available</p>
+
+        </section>
+
+        <section>
+        <h2 className="text-[#577D4E] font-semibold text-xl tracking-tight pb-4">Memories</h2>
+        <div className="mt-6">
+
+          <Timeline isLast={true} date="N/a" description="No history recorded"/>
+
+
+          
+          </div>
+        </section>
+
+        <section>
+        <h2 className="text-[#577D4E] font-semibold text-2xl tracking-tighter pb-2">Stats</h2>
+          <div className="grid grid-cols-2 gap-2">
+            <StatCard type="water" stat="Water Saved" measure="0L"/>
+            <StatCard type="cloud" stat="CO2 Saved  " measure="0kg"/>
+            <StatCard type="shirt" stat="times worn" measure="4"/>
+
+
+          </div>
+        </section>
         
 
         </main>
